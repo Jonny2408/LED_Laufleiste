f5f1d15ae9b2376cb64efd57d7914fe6ec0397d1
Jonny248 <Jonathan.S94@web.de>
2020-09-22 21:58:08 +0200

    Update LED_Laufleiste.ino
    Matrix und 2D Array hinzugefügt

--- LED_Laufleiste/LED_Laufleiste.ino
+++ LED_Laufleiste/LED_Laufleiste.ino
@@ -5,24 +5,35 @@
     Hardware: Arduion; LED Laufleiste an, LEDs von Pin 2-9
 */
 
-byte led[9]={2,3,4,5,6,7,8,9};
+byte led[9] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+byte ledZustand[6][8] =
+{
+  {1, 0, 0, 0, 0, 0, 0, 1},
+  {0, 1, 0, 0, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 1, 0, 0},
+  {0, 0, 0, 1, 1, 0, 0, 0},
+  {0, 0, 1, 0, 0, 1, 0, 0},
+  {0, 1, 0, 0, 0, 0, 1, 0},
+};
 
 void setup()
 {
-  for(byte i=0;i<8;i++)
+  for (byte i = 0; i < 8; i++)
   {
-    pinMode(led[i],OUTPUT);
+    pinMode(led[i], OUTPUT);
   }
 }
 
 void loop()
 {
-  for(byte i=0;i<8
-  ;i++)
+  for (byte i = 0; i < 6; i++) //Zeilen
   {
-    digitalWrite(led[i],HIGH);
-    delay(500);
-    digitalWrite(led[i],LOW);
+    delay(150);
+    for (byte j = 0; j < 8; j++) //Spalten
+    {
+
+      digitalWrite(led[j], ledZustand[i][j]);
+    }
   }
-  
 }
